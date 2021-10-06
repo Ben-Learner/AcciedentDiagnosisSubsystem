3995aeaef973e16f7d8d425bf0acf9c5478ffbe8
qiben-jy <qb19@mails.tsinghua.edu.cn>
2021-10-06 20:43:52 +0800

    get suitable output

--- pgmpy_test.py
+++ pgmpy_test.py
@@ -6,5 +6,4 @@ LOCA_inference_model = BeliefPropagation(LOCA_Bayesian_model) #construct inferen
 hard_evidence = {'AP':"higher"}
 virtual_evidence = TabularCPD(variable='RP', variable_card=3, values=[[0.1],[0.3],[0.6]])
 diagnosis_result = LOCA_inference_model.query(variables=['LLOCA'],evidence=hard_evidence,virtual_evidence=[virtual_evidence]) #go to inference
-print(diagnosis_result)
-
+print((diagnosis_result.values)[1])
