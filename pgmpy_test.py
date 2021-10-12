288e97fbb21bf034f049b4633e20354715c68d58
qiben-jy <qb19@mails.tsinghua.edu.cn>
2021-10-12 18:14:29 +0800

    type the Fuzzy function

--- pgmpy_test.py
+++ pgmpy_test.py
@@ -1,8 +1,7 @@
 from pgmpy.readwrite import BIFReader
 from pgmpy.inference import BeliefPropagation
 from pgmpy.factors.discrete import TabularCPD
-from pgmpy.models import DynamicBayesianNetwork as DBN
-from pgmpy.readwrite import BIFWriter
+
 
 class Accient_diagnosis:
     def __init__(self, path, hard_evidence, virtual_evidence, pre_diagnostic_accident):
@@ -16,28 +15,43 @@ class Accient_diagnosis:
         diagnosis_process = self.inference_model.query(variables=[self.pre_diagnostic_accident],
                                                        evidence=self.hard_evidence, virtual_evidence=[self.virtual_evidence])
         return (diagnosis_process.values)[1]
-
-
+class Fuzzy_function:
+    def __init__(self, real_time_value,lower_limit,upper_limit):
+        self.real_time_value = real_time_value
+        self.lower_limit = lower_limit
+        self.upper_limit = upper_limit
+        self.middle_value = 1/2*(self.upper_limit + self.lower_limit)
+    def low_probablity(self):
+        if self.real_time_value < self.upper_limit:
+            p_low = 1
+        elif self.lower_limit <= self.real_time_value <= self.middle_value:
+            p_low = (self.middle_value - self.real_time_value) / (self.middle_value - self.lower_limit)
+        else:
+            p_low = 0
+        return p_low
+    def normal_probablity(self):
+        if self.real_time_value < self.upper_limit:
+            p_normal = 0
+        elif self.lower_limit <= self.real_time_value <= self.middle_value:
+            p_normal = (self.real_time_value - self.lower_limit) / (self.middle_value - self.lower_limit)
+        elif self.middle_value < self.real_time_value <= self.upper_limit
+            p_normal = (self.upper_limit - self.real_time_value) / (self.upper_limit - self.middle_value)
+        else:
+            p_normal = 0
+        return p_normal
+    def high_probablity(self):
+        if self.real_time_value < self.middle_value:
+            p_high = 0
+        elif self.middle_value <= self.real_time_value <= self.upper_limit:
+            p_high = (self.real_time_value - self.middle_value) / (self.upper_limit - self.middle_value)
+        else:
+            p_high = 1
+        return p_high
+    def return_result(self):
 LOCA = Accient_diagnosis(path="./models/WU_LOCA.bif", hard_evidence={'CP':"higher"},
                          virtual_evidence=TabularCPD(variable='RP', variable_card=3, values=[[0.9],[0.1],[0]]),
                          pre_diagnostic_accident="LOCA")
 result = LOCA.inference()
 print(result)
-
-
-
-# LOCA_Bayesian_model = BIFReader("./models/WU_LOCA.bif").get_model() #read LOCA Bayesian model
-# SGTR_Bayesian_model = BIFReader("./models/WU_SGTR.bif").get_model() #read LOCA Bayesian model
-# LOCA_inference_model = BeliefPropagation(LOCA_Bayesian_model) #construct inference model
-# SGTR_inference_model = BeliefPropagation(SGTR_Bayesian_model) #construct inference model
-#
-# LOCA_hard_evidence = {'CP':"higher"}
-# LOCA_virtual_evidence = TabularCPD(variable='RP', variable_card=3, values=[[0.9],[0.1],[0]])
-# LOCA_diagnosis_result = LOCA_inference_model.query(variables=['LOCA'],evidence=LOCA_hard_evidence,virtual_evidence=[LOCA_virtual_evidence]) #go to inference
-# print((LOCA_diagnosis_result.values)[1])
-# SGTR_hard_evidence = {'PSGR':"normal"}
-# SGTR_virtual_evidence = TabularCPD(variable='RP', variable_card=3, values=[[0.9],[0.1],[0]])
-# SGTR_diagnosis_result = SGTR_inference_model.query(variables=['SGTR'],evidence=SGTR_hard_evidence,virtual_evidence=[SGTR_virtual_evidence]) #go to inference
-# print((SGTR_diagnosis_result.values)[1])
 
 
